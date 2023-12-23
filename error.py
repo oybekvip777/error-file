e50f2a22d87f30d229abbc0ae10e6c2472154579
oybek777 <153380033+oybekvip777@users.noreply.github.com>
2023-12-23 19:51:33 +0500

    Add files via upload

---
+++ error.py
@@ -0,0 +1,155 @@
+# 1-error
+
+# try:
+#     a=int(input('a='))
+#     b=int(input('b='))
+#     h=a/b
+#     print(h+10)
+
+
+# except ValueError:
+#     print('value error')    
+
+# 2-error
+
+# try:
+#     ism='oybek'
+#     print(ism3)
+
+# except NameError:
+#     print("o'zgaruvchi noto'g'ri")    
+
+# 3-error
+
+# try:
+    # index=[1,2,3,4,5]
+    # index1=index(7)
+    # print(index1)
+# except IndexError:
+    # print('index notogri')
+
+# 4-Error
+
+# try:
+
+#     print('komiljonov oybek'
+
+# except:
+#     print('syntax notogri ')
+
+# 5-Error
+
+# try:
+#     zero=int(input('a='))
+#     zero1=int(input('b='))
+#     dp=zero/zero1
+#     print(dp)
+# except:ZeroDivisionError
+# print('notogri son')
+
+# 6-error
+
+
+# try:
+#   with open('oybek1.py','r')as end:
+    # go=end.read()
+    # print(go)
+# except: FileNotFoundError:
+# print('fali')  
+
+# 7-error sariq dev
+
+# try:
+#     yosh=input("Yoshingizni kiriting: ")
+        
+# except:
+#     print("son kiritmadigiz")
+# else:
+#     print(f"Siz {2023-yosh} yilda tug'ilgansiz")
+
+# 8-error sariq dev
+
+# y=int(input('a='))
+# x=int(input ('b='))
+# try:
+#     x=int(input ('b='))
+#     y=int(input('a='))
+   
+#     y/x
+# except ZeroDivisionError:
+#     print("0 qoshish mumkinmas")
+
+# 9-error sariq dev
+
+# user = {"username":"sariqdev",
+#         "status":"admin",
+#         "email":"admin@sariq.dev",
+#         "phone":"99897123456"}
+
+# key="statiom"
+# try:
+#     print(f'Foydalanuvchi: {user[key]}')
+# except KeyError:
+#     print("Bunday key mavjud emas")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
